e136212497d73aed9eb06a7d968ec836822dd4d5
Vitor3362 <vitorhenrique095@gmail.com>
2025-06-09 04:59:11 +0000

    Super trunfo

--- SuperTrunfo.c
+++ SuperTrunfo.c
@@ -71,6 +71,6 @@ int main() {
     printf ("Área: %.2f km²\n", area2);
     printf ("PIB: %.2f bilhões de reais\n", pib2);
     printf ("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
+
     return 0;
 }
-
